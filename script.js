5b4e7dff9f587cb95e5ca4dce053431b2ac2c101
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-08-03 18:12:46 +0300

    watchlist problems fixed

--- script.js
+++ script.js
@@ -1,82 +1,140 @@
+// movie-container ve diğer öğeleri al
 const movieContainer = document.getElementById('movie-container');
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 const noData = document.getElementById('no-data');
+const watchlistContainer = document.getElementById('watchlist-container');
+const watchlistBody = document.getElementById('watchlist-body');
+const watchlistNoData = document.getElementById('watchlist-no-data');
 
-// movie-container içeriğini her bir film için oluştururken, add watchlist img'ye click event listener ekleyeceğiz.
-function fetchData() {
-    fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=f33ec139`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        if (data.Response === 'False') {
-            movieContainer.innerHTML = '<h2>No data found</h2>';
-            return;
-        }
+// index.html'de çalışacak kodlar
+if (movieContainer && searchButton && searchInput && noData) {
+    searchButton.addEventListener('click', fetchData);
 
-        // Clear previous results
-        movieContainer.innerHTML = '';
-
-        // Iterate over the 'Search' array
-        data.Search.forEach(movie => {
-            fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=f33ec139`)
+    function fetchData() {
+        fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=f33ec139`)
             .then(response => response.json())
-            .then(movieDetails => {
-                // Yeni bir movie div'i oluştur
-                const movieDiv = document.createElement('div');
-                movieDiv.classList.add('movie');
+            .then(data => {
+                console.log(data);
+                if (data.Response === 'False') {
+                    movieContainer.innerHTML = '<h2>No data found</h2>';
+                    return;
+                }
 
-                // movieDiv'in içeriğini oluştur
-                movieDiv.innerHTML = `
-                    <img class="poster" src="${movieDetails.Poster}" alt="${movieDetails.Title}">
-                    <div>
-                        <h3>${movieDetails.Title}</h3>
-                        <p><strong>Year:</strong> ${movieDetails.Year}</p>
-                        <p><strong>IMDb Rating ⭐:</strong> ${movieDetails.imdbRating}</p>
-                        <p><strong>Description:</strong> ${movieDetails.Plot}</p>
-                        <p><strong>Runtime:</strong> ${movieDetails.Runtime}</p>
-                        <div class="add-container">
-                            <p>Add Watchlist</p>
-                            <img class="add-icon" src="/assets/add.svg">
-                        </div>
-                    </div>
-                `;
+                movieContainer.innerHTML = ''; // Önceki sonuçları temizle
 
-                // movieDiv'e event listener ekle
-                const addIcon = movieDiv.querySelector('.add-icon');
-                addIcon.addEventListener('click', function() {
-                    // İcon'un src değerini değiştir
-                    if (addIcon.src.includes('add.svg')) {
-                        addIcon.src = '/assets/remove.svg';
-                    } else {
-                        addIcon.src = '/assets/add.svg';
-                    }
-                    // addToWatchlist fonksiyonuna movieDetails'i gönder
-                    addToWatchlist(movieDetails);
+                data.Search.forEach(movie => {
+                    fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=f33ec139`)
+                        .then(response => response.json())
+                        .then(movieDetails => {
+                            const movieDiv = document.createElement('div');
+                            movieDiv.classList.add('movie');
+
+                            movieDiv.innerHTML = `
+                                <img class="poster" src="${movieDetails.Poster}" alt="${movieDetails.Title}">
+                                <div>
+                                    <h3>${movieDetails.Title}</h3>
+                                    <p><strong>Year:</strong> ${movieDetails.Year}</p>
+                                    <p><strong>IMDb Rating ⭐:</strong> ${movieDetails.imdbRating}</p>
+                                    <p><strong>Description:</strong> ${movieDetails.Plot}</p>
+                                    <p><strong>Runtime:</strong> ${movieDetails.Runtime}</p>
+                                    <div class="add-container">
+                                        <p>Add Watchlist</p>
+                                        <img class="add-icon" src="/assets/add.svg">
+                                    </div>
+                                </div>
+                            `;
+
+                            const addIcon = movieDiv.querySelector('.add-icon');
+                            addIcon.addEventListener('click', function () {
+                                if (addIcon.src.includes('add.svg')) {
+                                    addToWatchlist(movieDetails);
+                                    addIcon.src = '/assets/remove.svg';
+                                } else {
+                                    removeToWatchlist(movieDetails);
+                                    addIcon.src = '/assets/add.svg';
+                                }
+                            });
+
+                            movieContainer.appendChild(movieDiv);
+                        })
+                        .catch(error => {
+                            console.error('Error fetching movie details:', error);
+                            movieContainer.innerHTML += '<h2>Error loading movie details</h2>';
+                        });
                 });
 
-                // movieDiv'i movieContainer'a ekle
-                movieContainer.appendChild(movieDiv);
+                searchInput.value = '';
+                noData.style.display = 'none';
             })
             .catch(error => {
-                console.error('Error fetching movie details:', error);
-                movieContainer.innerHTML += '<h2>Error loading movie details</h2>';
+                console.error('Error fetching data:', error);
+                movieContainer.innerHTML = '<h2>Error loading data</h2>';
             });
-        });
+    }
+}
 
-        searchInput.value = '';
-        noData.style.display = 'none';
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-        movieContainer.innerHTML = '<h2>Error loading data</h2>';
+// watchlist.html'de çalışacak kodlar
+if (watchlistContainer && watchlistBody && watchlistNoData) {
+    document.addEventListener('DOMContentLoaded', function () {
+        if (localStorage.length === 0) {
+            watchlistNoData.style.display = 'block';
+            return;
+        } else {
+            watchlistNoData.style.display = 'none';
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const movieKey = localStorage.key(i);
+            const movie = JSON.parse(localStorage.getItem(movieKey));
+
+            const movieDiv = document.createElement('div');
+            movieDiv.classList.add('movie');
+
+            movieDiv.innerHTML = `
+                <img class="poster" src="${movie.Poster}" alt="${movie.Title}">
+                <div>
+                    <h3>${movie.Title}</h3>
+                    <p><strong>Year:</strong> ${movie.Year}</p>
+                    <p><strong>IMDb Rating ⭐:</strong> ${movie.imdbRating}</p>
+                    <p><strong>Description:</strong> ${movie.Plot}</p>
+                    <p><strong>Runtime:</strong> ${movie.Runtime}</p>
+                    <div class="add-container">
+                        <p>Remove from Watchlist</p>
+                        <img class="remove-icon" src="/assets/remove.svg">
+                    </div>
+                </div>
+            `;
+
+            const removeIcon = movieDiv.querySelector('.remove-icon');
+            removeIcon.addEventListener('click', function () {
+                localStorage.removeItem(movie.Title);
+                movieDiv.remove();
+
+                if (localStorage.length === 0) {
+                    watchlistNoData.style.display = 'block';
+                }
+            });
+
+            watchlistBody.appendChild(movieDiv);
+        }
     });
 }
 
-searchButton.addEventListener('click', fetchData);
-
-// addToWatchlist fonksiyonu her movie için çağrılır
 function addToWatchlist(movie) {
-    console.log(movie.Title);
+    const movieObj = {
+        Title: movie.Title,
+        Year: movie.Year,
+        imdbRating: movie.imdbRating,
+        Plot: movie.Plot,
+        Runtime: movie.Runtime,
+        Poster: movie.Poster
+    };
+
+    localStorage.setItem(movieObj.Title, JSON.stringify(movieObj));
 }
 
+function removeToWatchlist(movie) {
+    localStorage.removeItem(movie.Title);
+    console.log('removeToWatchlist', movie);
+}
